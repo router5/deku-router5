51704858a9072d4dd8c9aac21c27f6d5fa4f1138
Thomas Roch <thomas.c.roch@gmail.com>
2015-07-10 11:24:16 +0100

    chore: add build script

---
+++ dist/commonjs/link-factory.js
@@ -0,0 +1,61 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _deku = require('deku');
+
+// Use of a factory rather than source as having nested routers
+// are not excluded
+exports['default'] = linkFactory;
+
+function linkFactory(router) {
+    var useSource = arguments[1] === undefined ? true : arguments[1];
+
+    var propTypes = {
+        button: { type: 'boolean' },
+        routeName: { type: 'string', optional: false },
+        routeParams: { type: 'object' },
+        routeOptions: { type: 'object' },
+        activeClass: { type: 'string' },
+        activeStrict: { type: 'function' },
+        onClick: { type: 'function' }
+    };
+
+    if (useSource) propTypes.currentRoute = { source: 'currentRoute' };
+
+    var defaultProps = {
+        activeClass: 'active',
+        button: false,
+        activeStrict: false,
+        routeParams: {},
+        routeOptions: {}
+    };
+
+    var clickHandler = function clickHandler(evt, component, updateState) {
+        evt.preventDefault();
+
+        var props = component.props;
+
+        router.navigate(props.routeName, props.routeParams, props.routeOptions);
+    };
+
+    var render = function render(component, el) {
+        var props = component.props;
+
+        var active = router.isActive(props.routeName, props.routeParams);
+        var href = router.buildUrl(props.routeName, props.routeParams);
+        var className = (props['class'] ? props['class'].split(' ') : []).concat(active ? [props.activeClass] : []).join(' ');
+
+        var onClick = props.onClick || clickHandler;
+
+        if (props.button) {
+            return (0, _deku.element)('button', { type: 'button', 'class': className, onClick: onClick }, props.children);
+        }
+        return (0, _deku.element)('a', { href: href, 'class': className, onClick: onClick }, props.children);
+    };
+
+    return { propTypes: propTypes, defaultProps: defaultProps, render: render };
+}
+module.exports = exports['default'];
--- modules/segment-decorator.js
+++ modules/segment-decorator.js
@@ -3,18 +3,18 @@ export default segmentDecoratorFactory
 function segmentDecoratorFactory(router) {
     let listeners = {}
 
-    return function segmentDecorator(componentDef, routeName, listener) {
-        componentDef.afterMount = (component, el, setState) => {
-            function getListener(component) {
-                listeners[component.id] = (toState, fromState) => {
-                    listener(component, toState, fromState)
-                }
-                return isteners[component.id]
+    return function SegmentDecorator(componentDef, routeName, listener) {
+        let nodeListener = (comp) => {
+            listeners[comp.id] = (toState, fromState) => {
+                listener(comp, toState, fromState)
             }
+            return listeners[comp.id]
+        }
 
+        componentDef.afterMount = (component, el, setState) => {
             if (componentDef.afterMount) componentDef.afterMount(component, el, setState)
 
-            if (listener) router.addNodeListener(routeName, this.nodeListener)
+            if (listener) router.addNodeListener(routeName, nodeListener(component))
 
             router.registerComponent(routeName, component)
         }
